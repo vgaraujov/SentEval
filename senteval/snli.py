b58408994ac4bb4abb680dac19ea4456dff8b901
kushaltatariya <103580859+kushaltatariya@users.noreply.github.com>
2023-08-16 00:19:55 +0200

    Update snli.py - saving embeds

--- senteval/snli.py
+++ senteval/snli.py
@@ -17,13 +17,13 @@ import copy
 import logging
 import numpy as np
 
-
 from senteval.tools.validation import SplitClassifier
 
 
 class SNLIEval(object):
     def __init__(self, taskpath, seed=1111):
         logging.debug('***** Transfer task : SNLI Entailment*****\n\n')
+        self.task_name = os.path.basename(task_path)
         self.seed = seed
         train1 = self.loadFile(os.path.join(taskpath, 's1.train'))
         train2 = self.loadFile(os.path.join(taskpath, 's2.train'))
@@ -40,6 +40,7 @@ class SNLIEval(object):
         test2 = self.loadFile(os.path.join(taskpath, 's2.test'))
         testlabels = io.open(os.path.join(taskpath, 'labels.test'),
                              encoding='utf-8').read().splitlines()
+        testindexes = list(range(len(testlabels)))
 
         # sort data (by s2 first) to reduce padding
         sorted_train = sorted(zip(train2, train1, trainlabels),
@@ -50,14 +51,14 @@ class SNLIEval(object):
                               key=lambda z: (len(z[0]), len(z[1]), z[2]))
         valid2, valid1, validlabels = map(list, zip(*sorted_valid))
 
-        sorted_test = sorted(zip(test2, test1, testlabels),
-                             key=lambda z: (len(z[0]), len(z[1]), z[2]))
-        test2, test1, testlabels = map(list, zip(*sorted_test))
+        sorted_test = sorted(zip(test2, test1, testlabels, testindexes),
+                             key=lambda z: (len(z[0]), len(z[1]), z[2], z[3]))
+        test2, test1, testlabels, testindexes = map(list, zip(*sorted_test))
 
         self.samples = train1 + train2 + valid1 + valid2 + test1 + test2
-        self.data = {'train': (train1, train2, trainlabels),
-                     'valid': (valid1, valid2, validlabels),
-                     'test': (test1, test2, testlabels)
+        self.data = {'train': (train1, train2, trainlabels, None),
+                     'valid': (valid1, valid2, validlabels, None),
+                     'test': (test1, test2, testlabels, testindexes)
                      }
 
     def do_prepare(self, params, prepare):
@@ -69,31 +70,72 @@ class SNLIEval(object):
                     f.read().splitlines()]
 
     def run(self, params, batcher):
-        self.X, self.y = {}, {}
+        self.X, self.y, self.index = {}, {}, {}
         dico_label = {'entailment': 0,  'neutral': 1, 'contradiction': 2}
-        for key in self.data:
-            if key not in self.X:
-                self.X[key] = []
-            if key not in self.y:
-                self.y[key] = []
-                
-            input1, input2, mylabels = self.data[key]
-            enc_input = []
-            n_labels = len(mylabels)
-            for ii in range(0, n_labels, params.batch_size):
-                batch1 = input1[ii:ii + params.batch_size]
-                batch2 = input2[ii:ii + params.batch_size]
 
-                if len(batch1) == len(batch2) and len(batch1) > 0:
-                    enc1 = batcher(params, batch1)
-                    enc2 = batcher(params, batch2)
-                    enc_input.append(np.hstack((enc1, enc2, enc1 * enc2,
-                                                np.abs(enc1 - enc2))))
-                if (ii*params.batch_size) % (20000*params.batch_size) == 0:
-                    logging.info("PROGRESS (encoding): %.2f%%" %
-                                 (100 * ii / n_labels))
-            self.X[key] = np.vstack(enc_input)
-            self.y[key] = np.array([dico_label[y] for y in mylabels])
+        if params.save_emb is not None:
+            data_filename = '_'.join(params.save_emb.split('_')[:-1]) + '_' + self.task_name + '.npz'
+            if os.path.isfile(data_filename):
+                logging.info('Loading sentence embeddings')
+                loaded_data = np.load(data_filename)
+                self.X, self.y, self.index = loaded_data['X'], loaded_data['y'], loaded_data['index']
+                logging.info('Generated sentence embeddings')
+            else:
+                for key in self.data:
+                    logging.info('Computing embedding for {0}'.format(key))
+                    if key not in self.X:
+                        self.X[key] = []
+                    if key not in self.y:
+                        self.y[key] = []
+
+                    input1, input2, mylabels, myindexes = self.data[key]
+                    enc_input = []
+                    n_labels = len(mylabels)
+                    for ii in range(0, n_labels, params.batch_size):
+                        batch1 = input1[ii:ii + params.batch_size]
+                        batch2 = input2[ii:ii + params.batch_size]
+
+                        if len(batch1) == len(batch2) and len(batch1) > 0:
+                            enc1 = batcher(params, batch1)
+                            enc2 = batcher(params, batch2)
+                            enc_input.append(np.hstack((enc1, enc2, enc1 * enc2,
+                                                        np.abs(enc1 - enc2))))
+                        if (ii*params.batch_size) % (20000*params.batch_size) == 0:
+                            logging.info("PROGRESS (encoding): %.2f%%" %
+                                         (100 * ii / n_labels))
+                    self.X[key] = np.vstack(enc_input)
+                    self.y[key] = np.array([dico_label[y] for y in mylabels])
+                    self.index[key] = np.array(myindexes)
+                    logging.info('Computed {0} embeddings'.format(key))
+                logging.info('Saving sentence embeddings')
+                np.savez(data_filename, X=self.X, y=self.y, index=self.index)
+        else:
+            for key in self.data:
+                logging.info('Computing embedding for {0}'.format(key))
+                if key not in self.X:
+                    self.X[key] = []
+                if key not in self.y:
+                    self.y[key] = []
+
+                input1, input2, mylabels, myindexes = self.data[key]
+                enc_input = []
+                n_labels = len(mylabels)
+                for ii in range(0, n_labels, params.batch_size):
+                    batch1 = input1[ii:ii + params.batch_size]
+                    batch2 = input2[ii:ii + params.batch_size]
+
+                    if len(batch1) == len(batch2) and len(batch1) > 0:
+                        enc1 = batcher(params, batch1)
+                        enc2 = batcher(params, batch2)
+                        enc_input.append(np.hstack((enc1, enc2, enc1 * enc2,
+                                                    np.abs(enc1 - enc2))))
+                    if (ii*params.batch_size) % (20000*params.batch_size) == 0:
+                        logging.info("PROGRESS (encoding): %.2f%%" %
+                                     (100 * ii / n_labels))
+                self.X[key] = np.vstack(enc_input)
+                self.y[key] = np.array([dico_label[y] for y in mylabels])
+                self.index[key] = np.array(myindexes)
+                logging.info('Computed {0} embeddings'.format(key))
 
         config = {'nclasses': 3, 'seed': self.seed,
                   'usepytorch': params.usepytorch,
