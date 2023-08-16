3d7977b2fa7896e8006dd11b2691fc16993f6648
kushaltatariya <103580859+kushaltatariya@users.noreply.github.com>
2023-08-16 10:52:52 +0200

    Update sick.py

--- senteval/sick.py
+++ senteval/sick.py
@@ -214,7 +214,7 @@ class SICKEntailmentEval(SICKRelatednessEval):
             data_filename = '_'.join(params.save_emb.split('_')[:-1]) + '_' + self.task_name + 'E' + '.npy'
             if os.path.isfile(data_filename):
                 logging.info('Loading sentence embeddings')
-                sick_embed = np.load(data_filename)
+                sick_embed = np.load(data_filename, allow_pickle = True)
                 logging.info('Generated sentence embeddings')
             else:
                 for key in self.sick_data:
