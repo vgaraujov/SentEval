ed4a820ede9cdd53b8b7e3f3804ad8e66ccb9e8c
Kushal <ktatariya98@gmail.com>
2024-02-22 16:02:11 +0100

    adding mt5

--- examples/layers.sh
+++ examples/layers.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#layers=(1 2 3 4 5 6 7 8 9 10 11 12 'all')
+layers=(1 2 3 4 5 6 7 8 9 10 11 12 'all')
 lang=$1 #1_Arabic, 2_Chinese, 3_Hebrew, 4_Hindi, 5_Russian, 6_Tamil
-pooling=$2
-layers=$3
+#layers=$3
 auth="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"
 
-#for layer in ${layers[@]}; do
- #       python transformers_bert.py --model_name bert --task_index $task --pooling $pooling --layer $layer
-#        wait
-#done
+for layer in ${layers[@]}; do
+        python transformers_bert.py --model_name mt5 --language $lang --pooling mean --layer $layer
+        wait
+done
 
-python transformers_pixel.py --model_name mpixel --language $lang --pooling $pooling --layer $layers --auth $auth
-
+#python transformers_pixel.py --model_name mpixel --language $lang --pooling $pooling --layer $layers --auth $auth
