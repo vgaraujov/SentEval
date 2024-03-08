43d34bf1079b146dfe311bb6e8666f6ab9e67b39
Kushal <ktatariya98@gmail.com>
2024-03-08 11:21:54 +0100

    slurm

--- examples/layers.sh
+++ examples/layers.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 layers=(1 2 3 4 5 6 7 8 9 10 11 12 'all')
-lang=$1 #1_Arabic, 2_Chinese, 3_Hebrew, 4_Hindi, 5_Russian, 6_Tamil
-#layers=$3
+lang=$1
+model=$2
+layer=$3
+
+if [ $model == "pixel" ]; then
+  script="transformers_pixel.py"
+elif [ $model == "bert" ]; then
+  script="transformers_bert.py"
+elif [ $model == "vit-mae" ]; then
+  script="transformers_pixel.py"
+elif [ $model == "mbert" ]; then
+  script="transformers_bert.py"
+elif [ $model == "xlmr" ]; then
+  script="transformers_bert.py"
+else
+  echo "Invalid model name"
+  exit 1
+fi
 
 
-#for layer in ${layers[@]}; do
-#        python transformers_bert.py --model_name mt5 --language $lang --pooling mean --layer $layer
-#        wait
-#done
-
-#python transformers_pixel.py --model_name mpixel --language $lang --pooling $pooling --layer $layers --auth $auth
-python transformers_bert.py --model_name mt5 --language $lang --pooling mean --layer all
+if [ $lang == "English"]; then
+  python $script --model_name $model --language $lang --pooling mean --layer $layer --task_index 4
+else
+  python $script --model_name $model --language $lang --pooling mean --layer $layer
+fi
