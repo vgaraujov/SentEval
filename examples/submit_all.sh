ac3f5c404a1517ce41d6a5bd761c1e7c7a68c447
Kushal <ktatariya98@gmail.com>
2024-03-14 18:07:40 +0100

    run pos on vit

---
+++ examples/submit_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sbatch genius_probe_3.slurm Chinese pixel 32873
+sbatch genius_probe_3.slurm Hebrew pixel 32873
+sbatch genius_probe_3.slurm Russian pixel 32873
+sbatch genius_probe_3.slurm Hindi pixel 32873
+sbatch genius_probe_3.slurm Arabic pixel 32873
+sbatch genius_probe_3.slurm Tamil pixel 32873
+sbatch genius_probe_3.slurm Sanskrit pixel 32873
+
