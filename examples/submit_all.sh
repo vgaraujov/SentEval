73726f7bedab72247a0b4a74260c1423eeb8b7ab
Kushal <ktatariya98@gmail.com>
2024-03-14 18:36:17 +0100

    run pos on vit

--- examples/submit_all.sh
+++ examples/submit_all.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-sbatch genius_auth_probe_3.slurm Chinese mpixel 32873
-sbatch genius_auth_probe_3.slurm Hebrew mpixel 32873
-sbatch genius_auth_probe_3.slurm Russian mpixel 32873
-sbatch genius_auth_probe_3.slurm Hindi mpixel 32873
-sbatch genius_auth_probe_3.slurm Arabic mpixel 32873
-sbatch genius_auth_probe_3.slurm Tamil mpixel 32873
-sbatch genius_auth_probe_3.slurm Sanskrit mpixel 32873
+sbatch genius_probe_auth_3.slurm Chinese mpixel 32873
+sbatch genius_probe_auth_3.slurm Hebrew mpixel 32873
+sbatch genius_probe_auth_3.slurm Russian mpixel 32873
+sbatch genius_probe_auth_3.slurm Hindi mpixel 32873
+sbatch genius_probe_auth_3.slurm Arabic mpixel 32873
+sbatch genius_probe_auth_3.slurm Tamil mpixel 32873
+sbatch genius_probe_auth_3.slurm Sanskrit mpixel 32873
 
-sbatch genius_auth_probe_3.slurm Chinese mpixel 8446
-sbatch genius_auth_probe_3.slurm Hebrew mpixel 8446
-sbatch genius_auth_probe_3.slurm Russian mpixel 8446
-sbatch genius_auth_probe_3.slurm Hindi mpixel 8446
-sbatch genius_auth_probe_3.slurm Arabic mpixel 8446
-sbatch genius_auth_probe_3.slurm Tamil mpixel 8446
-sbatch genius_auth_probe_3.slurm Sanskrit mpixel 8446
+sbatch genius_probe_auth_3.slurm Chinese mpixel 8446
+sbatch genius_probe_auth_3.slurm Hebrew mpixel 8446
+sbatch genius_probe_auth_3.slurm Russian mpixel 8446
+sbatch genius_probe_auth_3.slurm Hindi mpixel 8446
+sbatch genius_probe_auth_3.slurm Arabic mpixel 8446
+sbatch genius_probe_auth_3.slurm Tamil mpixel 8446
+sbatch genius_probe_auth_3.slurm Sanskrit mpixel 8446
+
 
 
 
