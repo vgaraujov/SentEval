ca5ba8b688320307f85b63e2d7be9ae8062482e4
Kushal <ktatariya98@gmail.com>
2024-03-28 08:55:16 +0100

    run pos on vit

--- examples/submit_all.sh
+++ examples/submit_all.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sbatch probe.slurm English pixel 8446
-sbatch probe.slurm English bert 8446
-sbatch probe.slurm English vit-mae 8446
+sbatch genius_probe.slurm English bert 8446
+sbatch genius_probe.slurm English vit-mae 8446
 
-sbatch probe.slurm English pixel 32873
-sbatch probe.slurm English bert 32873
-sbatch probe.slurm English vit-mae 32873
+sbatch genius_probe.slurm English pixel 32873
+sbatch genius_probe.slurm English bert 32873
+sbatch genius_probe.slurm English vit-mae 32873
 
