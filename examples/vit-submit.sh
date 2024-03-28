9fb9c7e0fee83119c6d6db52a73d24f02e8b422c
Kushal <ktatariya98@gmail.com>
2024-03-28 11:00:34 +0100

    run pos on vit

--- examples/vit-submit.sh
+++ examples/vit-submit.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-sbatch vit-finetuning.slurm UD_Arabic-PADT 2f9506e16930f137abbd18a3fb16f6b31840a830
-sbatch vit-finetuning.slurm UD_Korean-GSD 2f9506e16930f137abbd18a3fb16f6b31840a830
-sbatch vit-finetuning.slurm UD_Japanese-GSD 2f9506e16930f137abbd18a3fb16f6b31840a830
-sbatch vit-finetuning.slurm UD_Chinese-GSD 2f9506e16930f137abbd18a3fb16f6b31840a830
-sbatch vit-finetuning.slurm UD_Tamil-TTB 2f9506e16930f137abbd18a3fb16f6b31840a830
-sbatch vit-finetuning.slurm UD_Hindi-HDTB 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm dep UD_Arabic-PADT 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm dep UD_Korean-GSD 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm dep UD_Japanese-GSD 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm dep UD_Chinese-GSD 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm dep UD_Tamil-TTB 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm dep UD_Hindi-HDTB 2f9506e16930f137abbd18a3fb16f6b31840a830
+
+sbatch vit-finetuning.slurm ner ner/amh 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/hau 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/ibo 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/kin 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/lug 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/luo 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/pcm 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/swa 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/yor 2f9506e16930f137abbd18a3fb16f6b31840a830
+sbatch vit-finetuning.slurm ner ner/wol 2f9506e16930f137abbd18a3fb16f6b31840a830
+
