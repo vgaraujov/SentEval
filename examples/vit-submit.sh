51c321e26bac8d300543d54def79e9298864fb30
Kushal <ktatariya98@gmail.com>
2024-04-02 10:25:04 +0200

    more exps

--- examples/vit-submit.sh
+++ examples/vit-submit.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
 
-sbatch vit-finetuning.slurm dep UD_Arabic-PADT
-sbatch vit-finetuning.slurm dep UD_Korean-GSD
-sbatch vit-finetuning.slurm dep UD_Japanese-GSD
-sbatch vit-finetuning.slurm dep UD_Chinese-GSD
-sbatch vit-finetuning.slurm dep UD_Tamil-TTB
-sbatch vit-finetuning.slurm dep UD_Hindi-HDTB
+sbatch vit-finetuning.slurm dep UD_Vietnamese-VTB
+sbatch vit-finetuning.slurm dep UD_Coptic-Scriptorium
+sbatch vit-finetuning.slurm pos UD_Coptic-Scriptorium
 
-sbatch vit-finetuning.slurm ner ner/amh
-sbatch vit-finetuning.slurm ner ner/hau
-sbatch vit-finetuning.slurm ner ner/ibo
-sbatch vit-finetuning.slurm ner ner/kin
-sbatch vit-finetuning.slurm ner ner/lug
-sbatch vit-finetuning.slurm ner ner/luo
-sbatch vit-finetuning.slurm ner ner/pcm
-sbatch vit-finetuning.slurm ner ner/swa
-sbatch vit-finetuning.slurm ner ner/yor
-sbatch vit-finetuning.slurm ner ner/wol
+sbatch random-model.slurm pos UD_Vietnamese-VTB
+sbatch random-model.slurm pos UD_Coptic-Scriptorium
 
