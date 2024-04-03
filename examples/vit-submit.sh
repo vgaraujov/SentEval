5d4df3c111a7c23ee9849e2508d1e50ff32d3e7d
Kushal <ktatariya98@gmail.com>
2024-04-03 12:04:26 +0200

    random ner

--- examples/vit-submit.sh
+++ examples/vit-submit.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 
-sbatch vit-finetuning.slurm dep UD_Vietnamese-VTB
-sbatch vit-finetuning.slurm dep UD_Coptic-Scriptorium
-sbatch vit-finetuning.slurm pos UD_Coptic-Scriptorium
+sbatch random-model.slurm ner ner/amh
+sbatch random-model.slurm ner ner/hau
+sbatch random-model.slurm ner ner/ibo
+sbatch random-model.slurm ner ner/kin
+sbatch random-model.slurm ner ner/lug
+sbatch random-model.slurm ner ner/luo
+sbatch random-model.slurm ner ner/pcm
+sbatch random-model.slurm ner ner/swa
+sbatch random-model.slurm ner ner/wol
+sbatch random-model.slurm ner ner/yor
+sbatch random-model.slurm ner ner/zh
 
-sbatch random-model.slurm pos UD_Vietnamese-VTB
-sbatch random-model.slurm pos UD_Coptic-Scriptorium
+
 
